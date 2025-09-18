ee2c2bf36a35d76b27584ef73679a360bc16ead6
vinicius-goess <viny_lolo@hotmail.com>
2025-09-18 17:01:19 -0300

    Novos botões

--- SistemaEstoque/FormListagem.Designer.cs
+++ SistemaEstoque/FormListagem.Designer.cs
@@ -29,19 +29,75 @@ namespace SistemaEstoque
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvProdutos = new System.Windows.Forms.DataGridView();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvProdutos
+            // 
+            this.dgvProdutos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProdutos.Location = new System.Drawing.Point(31, 34);
+            this.dgvProdutos.Name = "dgvProdutos";
+            this.dgvProdutos.Size = new System.Drawing.Size(373, 195);
+            this.dgvProdutos.TabIndex = 0;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.Location = new System.Drawing.Point(31, 320);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(113, 83);
+            this.btnEditar.TabIndex = 1;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.Location = new System.Drawing.Point(160, 320);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(113, 83);
+            this.btnExcluir.TabIndex = 2;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAtualizar.Location = new System.Drawing.Point(291, 320);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(113, 83);
+            this.btnAtualizar.TabIndex = 3;
+            this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
             // 
             // FormListagem
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(442, 507);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.dgvProdutos);
             this.Name = "FormListagem";
             this.Text = "Listagem";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvProdutos;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnAtualizar;
     }
 }
--- SistemaEstoque/FormListagem.cs
+++ SistemaEstoque/FormListagem.cs
@@ -7,14 +7,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.ComponentModel;
 
 namespace SistemaEstoque
 {
     public partial class FormListagem : Form
     {
+        private BindingSource bs = new BindingSource();
         public FormListagem()
         {
             InitializeComponent();
+            MontarGrid();
+            AtualizarGrid();
+        }
+        private void MontarGrid()
+        {
+            dgvProdutos.AutoGenerateColumns = false;
+            dgvProdutos.Columns.Clear();
+            dgvProdutos.Columns.Add(new DataGridViewTextBoxColumn { Name = "Nome", HeaderText = "Nome", DataPropertyName = "Nome", AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill });
+            dgvProdutos.Columns.Add(new DataGridViewTextBoxColumn { Name = "Quantidade", HeaderText = "Quantidade", DataPropertyName = "Quantidade" });
+            dgvProdutos.Columns.Add(new DataGridViewTextBoxColumn { Name = "Preco", HeaderText = "Preço", DataPropertyName = "Preco", DefaultCellStyle = { Format = "C2" } });
+            dgvProdutos.Columns.Add(new DataGridViewTextBoxColumn { Name = "Categoria", HeaderText = "Categoria", DataPropertyName = "Categoria" });
+        }
+
+        private void AtualizarGrid()
+        {
+            bs.DataSource = Banco.Produtos;
+            dgvProdutos.DataSource = bs;
+            dgvProdutos.Refresh();
+        }
+
+        private Produto GetSelecionado()
+        {
+            if (dgvProdutos.CurrentRow == null) return null;
+            return dgvProdutos.CurrentRow.DataBoundItem as Produto;
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            var p = GetSelecionado();
+            if (p == null) { MessageBox.Show("Selecione um produto."); return; }
+            using (var f = new FormCadastro(p))
+            {
+                f.ShowDialog();
+            }
+            AtualizarGrid();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            var p = GetSelecionado();
+            if (p == null) { MessageBox.Show("Selecione um produto."); return; }
+            if (MessageBox.Show($"Excluir {p.Nome}?", "Confirma", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Banco.Produtos.Remove(p);
+                AtualizarGrid();
+            }
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            AtualizarGrid();
         }
     }
 }
